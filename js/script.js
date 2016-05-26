93a98051cb1870c3755127c9c1d3070e794c16da
Ftoroplast <shpindler.ar@gmail.com>
2016-05-26 18:43:35 +0300

    Начато создание кастомного скроллбара

--- js/script.js
+++ js/script.js
@@ -1,8 +1,43 @@
-$(function()
-{
-	$('.gallery').jScrollPane(
-    {
-      showArrows: true,
-      horizontalGutter: 18
+// $(function()
+// {
+//   var galleryPortfolioJsp =	$('.gallery--portfolio').jScrollPane(
+//     {
+//       showArrows: true,
+// 			autoReinitialise: true
+//     });
+//
+// 		var galleryClientsJsp =	$('.gallery--clients').jScrollPane(
+// 	    {
+// 	      showArrows: true,
+// 				autoReinitialise: true
+// 	    });
+// });
+
+(function () {
+  var galleries = document.querySelectorAll(".gallery");
+  var scrolls = document.querySelectorAll(".scroll");
+  var scrollArrowsLeft = document.querySelectorAll(".scroll__arrow--left");
+  var scrollArrowsRight = document.querySelectorAll(".scroll__arrow--right");
+  var galleryContainers = document.querySelectorAll(".gallery__container");
+
+  for (var i = 0; i < galleries.length; ++i) {
+    galleries[i].style.overflow = "hidden";
+  }
+
+  for (i = 0; i < scrolls.length; ++i) {
+    scrolls[i].style.display = "block";
+  }
+
+  for (let i = 0; i < scrollArrowsLeft.length; ++i) {
+    scrollArrowsLeft[i].addEventListener("click", function () {
+      galleryContainers[i].style.left = parseFloat(getComputedStyle(galleryContainers[i]).left) - 5 + "px";
     });
-});
+  }
+
+  for (let i = 0; i < scrollArrowsRight.length; ++i) {
+    scrollArrowsRight[i].addEventListener("click", function () {
+      galleryContainers[i].style.left = parseFloat(getComputedStyle(galleryContainers[i]).left) + 5 + "px";
+    });
+  }
+
+})();
