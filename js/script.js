7b44ce78da763115b9b2ef5f5d8765e0b43d98a9
Ftoroplast <shpindler.ar@gmail.com>
2016-05-27 01:11:12 +0300

    Доделаны стрелочки и поставлено ограничение на скроллинг. Остановился на оживление трека. Скрипт недописан, т.е. нерабочий.

--- js/script.js
+++ js/script.js
@@ -19,24 +19,43 @@
   var scrollArrowsLeft = document.querySelectorAll(".scroll__arrow--left");
   var scrollArrowsRight = document.querySelectorAll(".scroll__arrow--right");
   var galleryContainers = document.querySelectorAll(".gallery__container");
+  var scrollTracks = document.querySelectorAll(".scroll__track");
 
-  for (var i = 0; i < galleries.length; ++i) {
+  for (let i = 0; i < galleries.length; ++i) {
     galleries[i].style.overflow = "hidden";
   }
 
-  for (i = 0; i < scrolls.length; ++i) {
+  for (let i = 0; i < scrolls.length; ++i) {
     scrolls[i].style.display = "block";
   }
 
   for (let i = 0; i < scrollArrowsLeft.length; ++i) {
     scrollArrowsLeft[i].addEventListener("click", function () {
-      galleryContainers[i].style.left = parseFloat(getComputedStyle(galleryContainers[i]).left) - 5 + "px";
+      if (parseFloat(getComputedStyle(galleryContainers[i]).left) < 0) {
+        galleryContainers[i].style.left = parseFloat(getComputedStyle(galleryContainers[i]).left) + 5 + "px";
+      }
     });
   }
 
   for (let i = 0; i < scrollArrowsRight.length; ++i) {
     scrollArrowsRight[i].addEventListener("click", function () {
-      galleryContainers[i].style.left = parseFloat(getComputedStyle(galleryContainers[i]).left) + 5 + "px";
+      if (parseFloat(getComputedStyle(galleryContainers[i]).left) > galleries[i].offsetWidth - parseFloat(getComputedStyle(galleryContainers[i]).width)) {
+        galleryContainers[i].style.left = parseFloat(getComputedStyle(galleryContainers[i]).left) - 5 + "px";
+      }
+    });
+  }
+
+  for (let i = 0; i < scrollTracks.length; ++i) {
+    scrollTracks[i].addEventListener("mousedown", function () {
+      scrollTracks[i].addEventListener("mouseover", function trackMove() {
+
+      })
+    });
+
+    scrollTracks[i].addEventListener("mouseup", function () {
+      scrollTracks[i].removeEventListener("mouseover", function trackMove() {
+
+      })
     });
   }
 
