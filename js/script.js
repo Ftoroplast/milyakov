f4396690a004ceb31490999f0d7ac7d9671c1e64
Ftoroplast <shpindler.ar@gmail.com>
2016-05-28 19:55:21 +0300

    ВыЗавершена синхронизация трэка с галереей

--- js/script.js
+++ js/script.js
@@ -25,34 +25,28 @@
   var timerTrackToRight;
   var timerTrackToLeft;
   var timerTrackMove;
-  var currentPosition;
-
-  for (let i = 0; i < galleries.length; ++i) {
-    galleries[i].style.overflow = "hidden";
-  }
-
-  for (let i = 0; i < scrolls.length; ++i) {
-    scrolls[i].style.display = "block";
-  }
 
   setInterval(function () {
     for (let i = 0; i < scrolls.length; ++i) {
-      scrollTracks[i].style.width = scrolls[i].offsetWidth / galleryContainers[i].offsetWidth * galleries[i].offsetWidth + "px";
+      scrollTracks[i].style.width = (scrolls[i].offsetWidth - 30) / galleryContainers[i].offsetWidth * galleries[i].offsetWidth + "px";
     }
   }, 4);
 
-  for (let i = 0; i < scrollArrowsLeft.length; ++i) {
+  for (let i = 0; i < galleries.length; ++i) {
+    galleries[i].style.overflow = "hidden";
+    scrolls[i].style.display = "block";
+
     scrollArrowsLeft[i].addEventListener("mousedown", function () {
       timerOnLeftArrow = setInterval(function () {
-        if (parseFloat(getComputedStyle(galleryContainers[i]).left) < 0) {
-          galleryContainers[i].style.left = parseFloat(getComputedStyle(galleryContainers[i]).left) + 1 + "px";
+        if (parseFloat(getComputedStyle(galleryContainers[i]).left) < -3) {
+          galleryContainers[i].style.left = parseFloat(getComputedStyle(galleryContainers[i]).left) + (galleryContainers[i].offsetWidth - galleries[i].offsetWidth) / (scrolls[i].offsetWidth - scrollTracks[i].offsetWidth - 30) + "px";
         }
       }, 4);
     });
 
     scrollArrowsLeft[i].addEventListener("mousedown", function () {
       timerTrackToLeft = setInterval(function () {
-        if (parseFloat(getComputedStyle(scrollTracks[i]).left) > 19) {
+        if (parseFloat(getComputedStyle(scrollTracks[i]).left) > 15) {
           scrollTracks[i].style.left = parseFloat(getComputedStyle(scrollTracks[i]).left) - 1 + "px";
         }
       }, 4);
@@ -62,20 +56,18 @@
       clearInterval(timerOnLeftArrow);
       clearInterval(timerTrackToLeft);
     });
-  }
 
-  for (let i = 0; i < scrollArrowsRight.length; ++i) {
     scrollArrowsRight[i].addEventListener("mousedown", function () {
       timerOnRightArrow = setInterval(function () {
         if (parseFloat(getComputedStyle(galleryContainers[i]).left) > galleries[i].offsetWidth - parseFloat(getComputedStyle(galleryContainers[i]).width)) {
-          galleryContainers[i].style.left = parseFloat(getComputedStyle(galleryContainers[i]).left) - 1 + "px";
+          galleryContainers[i].style.left = parseFloat(getComputedStyle(galleryContainers[i]).left) - (galleryContainers[i].offsetWidth - galleries[i].offsetWidth) / (scrolls[i].offsetWidth - scrollTracks[i].offsetWidth - 30) + "px";
         }
       }, 4);
     });
 
     scrollArrowsRight[i].addEventListener("mousedown", function () {
       timerTrackToRight = setInterval(function () {
-        if (parseFloat(getComputedStyle(scrollTracks[i]).left) < scrolls[i].offsetWidth - scrollTracks[i].offsetWidth - 19) {
+        if (parseFloat(getComputedStyle(scrollTracks[i]).left) < scrolls[i].offsetWidth - scrollTracks[i].offsetWidth - 15) {
           scrollTracks[i].style.left = parseFloat(getComputedStyle(scrollTracks[i]).left) + 1 + "px";
         }
       }, 4);
@@ -87,23 +79,19 @@
     });
   }
 
-  for (let i = 0; i < scrollTracks.length; ++i) {
-    scrollTracks[i].addEventListener("mousedown", function () {
-      scrollTracks[i].addEventListener("mousemove", trackMove);
-    });
-
-    scrollTracks[i].addEventListener("mouseup", function () {
-      scrollTracks[i].removeEventListener("mousemove", trackMove);
-    });
-
-    function trackMove(event) {
-        setTimeout(function () {
-          currentPosition = event.clientX;
-        }, 4);
-        if (parseFloat(getComputedStyle(scrollTracks[i]).left) > 0 && parseFloat(getComputedStyle(scrollTracks[i]).left) < scrolls[i].offsetWidth - scrollTracks[i].offsetWidth - 19) {
-          scrollTracks[i].style.left = parseFloat(getComputedStyle(scrollTracks[i]).left) + event.clientX - currentPosition + "px";
-      }
-    }
-  }
-
+  // for (let i = 0; i < scrollTracks.length; ++i) {
+  //   scrollTracks[i].addEventListener("mousedown", function (e) {
+  //     document.addEventListener("mousemove", trackMove);
+  //   });
+  //
+  //   scrollTracks[i].addEventListener("mouseup", function () {
+  //     document.removeEventListener("mousemove", trackMove);
+  //   });
+  //
+  //   function trackMove(event) {
+  //       if (parseFloat(getComputedStyle(scrollTracks[i]).left) > -3 && parseFloat(getComputedStyle(scrollTracks[i]).left) < scrolls[i].offsetWidth - scrollTracks[i].offsetWidth - 15) {
+  //         scrollTracks[i].style.left =
+  //     }
+  //   }
+  // }
 })();
