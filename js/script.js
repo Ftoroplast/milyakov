7860976b35ba6a75a0cb3f8456d762ad37957579
Ftoroplast <shpindler.ar@gmail.com>
2016-05-29 12:49:15 +0300

    Скроллбар готов

--- js/script.js
+++ js/script.js
@@ -1,12 +1,12 @@
 // $(function()
 // {
-//   var galleryPortfolioJsp =	$('.gallery--portfolio').jScrollPane(
+//   var galleryPortfolioJsp =	$(".gallery--portfolio").jScrollPane(
 //     {
 //       showArrows: true,
 // 			autoReinitialise: true
 //     });
 //
-// 		var galleryClientsJsp =	$('.gallery--clients').jScrollPane(
+// 		var galleryClientsJsp =	$(".gallery--clients").jScrollPane(
 // 	    {
 // 	      showArrows: true,
 // 				autoReinitialise: true
@@ -26,12 +26,13 @@
   var timerTrackToLeft;
   var timerTrackMove;
   var shiftX = [];
-  var topTrackCoord = [];
-  var leftTrackCoord = [];
 
   setInterval(function () {
     for (let i = 0; i < scrolls.length; ++i) {
       scrollTracks[i].style.width = (scrolls[i].offsetWidth - 30) / galleryContainers[i].offsetWidth * galleries[i].offsetWidth + "px";
+      if (parseFloat(getComputedStyle(galleryContainers[i]).left) <= 0 || parseFloat(getComputedStyle(galleryContainers[i]).left) >= galleries[i].offsetWidth - galleryContainers[i].offsetWidth) {
+        galleryContainers[i].style.left = (getCoords(scrolls[i]).left + 15 - getCoords(scrollTracks[i]).left) * (galleryContainers[i].offsetWidth - galleries[i].offsetWidth) / (scrolls[i].offsetWidth - scrollTracks[i].offsetWidth - 30) + "px";
+      }
     }
   }, 4);
 
@@ -40,76 +41,70 @@
     scrolls[i].style.display = "block";
 
     scrollArrowsLeft[i].addEventListener("mousedown", function () {
-      timerOnLeftArrow = setInterval(function () {
-        if (parseFloat(getComputedStyle(galleryContainers[i]).left) < -3) {
-          galleryContainers[i].style.left = parseFloat(getComputedStyle(galleryContainers[i]).left) + (galleryContainers[i].offsetWidth - galleries[i].offsetWidth) / (scrolls[i].offsetWidth - scrollTracks[i].offsetWidth - 30) + "px";
-        }
-      }, 4);
-    });
-
-    scrollArrowsLeft[i].addEventListener("mousedown", function () {
       timerTrackToLeft = setInterval(function () {
-        if (parseFloat(getComputedStyle(scrollTracks[i]).left) > 15) {
+        if (getCoords(scrollTracks[i]).left >= 10 + getCoords(scrolls[i]).left) {
           scrollTracks[i].style.left = parseFloat(getComputedStyle(scrollTracks[i]).left) - 1 + "px";
         }
       }, 4);
     });
 
     scrollArrowsLeft[i].addEventListener("mouseup", function () {
-      clearInterval(timerOnLeftArrow);
       clearInterval(timerTrackToLeft);
     });
 
     scrollArrowsRight[i].addEventListener("mousedown", function () {
-      timerOnRightArrow = setInterval(function () {
-        if (parseFloat(getComputedStyle(galleryContainers[i]).left) > galleries[i].offsetWidth - parseFloat(getComputedStyle(galleryContainers[i]).width)) {
-          galleryContainers[i].style.left = parseFloat(getComputedStyle(galleryContainers[i]).left) - (galleryContainers[i].offsetWidth - galleries[i].offsetWidth) / (scrolls[i].offsetWidth - scrollTracks[i].offsetWidth - 30) + "px";
-        }
-      }, 4);
-    });
-
-    scrollArrowsRight[i].addEventListener("mousedown", function () {
       timerTrackToRight = setInterval(function () {
-        if (parseFloat(getComputedStyle(scrollTracks[i]).left) < scrolls[i].offsetWidth - scrollTracks[i].offsetWidth - 15) {
+        if (getCoords(scrollTracks[i]).left <= getCoords(scrolls[i]).left + scrolls[i].offsetWidth - scrollTracks[i].offsetWidth - 18) {
           scrollTracks[i].style.left = parseFloat(getComputedStyle(scrollTracks[i]).left) + 1 + "px";
         }
       }, 4);
     });
 
     scrollArrowsRight[i].addEventListener("mouseup", function () {
-      clearInterval(timerOnRightArrow);
       clearInterval(timerTrackToRight);
     });
 
-    scrollTracks[i].addEventListener("mousedown", function (event) {
-      // currentTrackPosition = parseFloat(getComputedStyle(scrollTracks[i]).left) + parseFloat(getComputedStyle(scrolls[i]).left);
-      // shiftX = event.pageX - currentTrackPosition;
-      topTrackCoord[i] = getCoords(scrollTracks[i]).top;
-      leftTrackCoord[i] = getCoords(scrollTracks[i]).left;
+    scrollTracks[i].onmousedown = function(e) {
+      document.onmousedown = document.onselectstart = function() {
+        return false;
+      };
+
+      var coords = getCoords(scrollTracks[i]);
+      var shiftX = e.pageX - coords.left;
+      var shiftY = e.pageY - coords.top
+
       document.body.appendChild(scrollTracks[i]);
-      scrollTracks[i].style.top = topTrackCoord[i] + "px";
-      scrollTracks[i].style.left = leftTrackCoord[i] + 4 + "px";
-      shiftX[i] = event.pageX - leftTrackCoord[i];
-      scrollTracks[i].addEventListener("mousemove", trackMove);
-    });
+      scrollTracks[i].style.left = e.pageX - shiftX + 4 + "px";
+      setInterval(function () {
+        scrollTracks[i].style.top = getCoords(scrolls[i]).top + "px";
+      }, 4);
 
-    document.addEventListener("mouseup", function (event) {
-      scrolls[i].appendChild(scrollTracks[i]);
-      scrollTracks[i].style.left = getCoords(scrollTracks[i]).left - getCoords(scrolls[i]).left + "px";
-      scrollTracks[i].style.top = 0 + "px";
-      scrollTracks[i].removeEventListener("mousemove", trackMove);
-    });
-
-    function trackMove() {
-        // if ((event.pageX - shiftX - currentTrackPosition >= 15 || event.pageX - shiftX - currentTrackPosition <= scrolls[i].offsetWidth - scrollTracks[i].offsetWidth - 15) && parseFloat(getComputedStyle(scrollTracks[i]).left) >= 15 && parseFloat(getComputedStyle(scrollTracks[i]).left) <= scrolls[i].offsetWidth - scrollTracks[i].offsetWidth - 15 ||
-        //     parseFloat(getComputedStyle(scrollTracks[i]).left) > scrolls[i].offsetWidth - scrollTracks[i].offsetWidth - 15 && (event.pageX - shiftX) < parseFloat(getComputedStyle(scrollTracks[i]).left) ||
-        //     parseFloat(getComputedStyle(scrollTracks[i]).left) < 15 && (event.pageX - shiftX) > parseFloat(getComputedStyle(scrollTracks[i]).left)) {
-        //   scrollTracks[i].style.left = event.pageX - shiftX - parseFloat(getComputedStyle(scrollTracks[i]).left) + parseFloat(getComputedStyle(scrolls[i]).left) + "px";
-        // }
-        if (parseFloat(getComputedStyle(scrollTracks[i]).left) >= parseFloat(getComputedStyle(scrolls[i]).left) + 15 && parseFloat(getComputedStyle(scrollTracks[i]).left) <= scrolls[i].offsetWidth - scrollTracks[i].offsetWidth - 15) {
-          scrollTracks[i].style.left = event.pageX - shiftX[i] + "px";
+      function moveXAt(e) {
+        if (e.pageX - shiftX + 4 < getCoords(scrolls[i]).left + 14) {
+          scrollTracks[i].style.left = getCoords(scrolls[i]).left + 14 + "px";
+        } else if (e.pageX - shiftX + 4 > getCoords(scrolls[i]).left + scrolls[i].offsetWidth - scrollTracks[i].offsetWidth - 14) {
+          scrollTracks[i].style.left = getCoords(scrolls[i]).left + scrolls[i].offsetWidth - scrollTracks[i].offsetWidth - 14 + "px";
+        } else {
+          scrollTracks[i].style.left = e.pageX - shiftX + "px";
         }
+      }
+
+      document.onmousemove = function(e) {
+        moveXAt(e);
+      };
+
+      document.onmouseup = function() {
+        document.onmousedown = document.onselectstart = function() {
+          return true;
+        };
+        document.onmousemove = null;
+        document.onmouseup = null;
+      };
     }
+
+    scrollTracks[i].ondragstart = function() {
+      return false;
+    };
   }
 
   function getCoords(elem) { // кроме IE8-
